cf62508d4e5d24861f38fa521e486bd8abd78d67
George-Markas <georgemarkanton@proton.me>
2023-05-24 15:26:19 +0300

    changes

--- demo_2.cpp
+++ demo_2.cpp
@@ -14,24 +14,26 @@ class Subject {
 
     public:
         // Generic constructor, to be used in Student class.
-        Subject(string password, string subjectName, unsigned int weekHours, bool season) {
-            this->password = std::move(password);
-            this->subjectName = std::move(subjectName);
-            this->weekHours = weekHours;
-            this->season = season;
+        Subject() {
+            password = "needsSetting";
+            subjectName = "needsNaming";
+            weekHours = 0;
+            season = false;
         }
 };
 
+list <Subject> subjectDB; // Assuming this is already filled out with the available courses.
+
 class Student {
     private:
-        char * registerNumber;
+        char *registerNumber;
         string fullName;
         unsigned int semester;
-        list<Subject> subjectList;
+        list<pair<Subject*, int>> subjectList; // int Grade, pointer to subject in subjectDB
 
     public:
         /* Setters */
-        void setRegisterNumber(char * registerNumber) {
+        void setRegisterNumber(char *registerNumber) {
             this->registerNumber = registerNumber;
         }
 
@@ -44,54 +46,107 @@ class Student {
         }
 
         /* Getters */
-        char * getRegisterNumber() {
+        char *getRegisterNumber() {
             return registerNumber;
         }
 
         string getFullName() {
-            return fullName;
+             return fullName;
         }
 
         [[nodiscard]] unsigned int getSemester() const {
-            return semester;
+             return semester;
         }
 
         /* Constructors */
-        Student(char* registerNumber, string fullName) {
+        Student(char *registerNumber, string fullName) {
             this->registerNumber = registerNumber;
             this->fullName = std::move(fullName);
             semester = 1;
         }
 
-        Student(char* registerNumber, string fullName, unsigned int semester) {
+        Student(char *registerNumber, string fullName, unsigned int semester) {
             this->registerNumber = registerNumber;
             this->fullName = std::move(fullName);
             this->semester = semester;
         }
 
-        Student(Student &that) {
-            registerNumber = that.registerNumber;
-            fullName  = that.fullName;
-            semester = that.semester;
+         Student(Student &that) {
+             registerNumber = that.registerNumber;
+             fullName = that.fullName;
+             semester = that.semester;
         }
 
         /* Class Functions */
         void attributePrint() {
-            std::cout << "Register Number: " << registerNumber << " | Full Name: " << fullName << " | Semester: "<< semester;
+            std::cout << "Register Number: " << registerNumber << " | Full Name: " << fullName << " | Semester: " << semester;
         }
 
-        void operator+=(unsigned int increment) {
-            semester += increment;
-        }
-        void operator-=(unsigned int decrement) {
-            semester -= decrement;
+        void operator++() {
+            semester++;
         }
 
-        void operator+=(::nullptr_t) {
-            subjectList.push_front(Subject()) {
+        void operator--() {
+            semester--;
+        }
+
+        void operator+=(unsigned int subjectNumber) {
+            for(int i = 0; i < subjectNumber; i++) {
+                subjectList.emplace_front();
+            }
+        }
+
+        bool operator==(unsigned int semesterNumber) const {
+            if (semester == semesterNumber) {
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
+
+        bool operator!=(unsigned int semesterNumber) const {
+            if (semester != semesterNumber) {
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
+
+        bool operator<(unsigned int semesterNumber) const {
+            if (semester < semesterNumber) {
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
+
+        bool operator>(unsigned int semesterNumber) const {
+            if (semester > semesterNumber) {
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
+
+        bool operator<=(unsigned int semesterNumber) const {
+            if (semester <= semesterNumber) {
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
+
+        bool operator>=(unsigned int semesterNumber) const {
+            if (semester >= semesterNumber) {
+                return true;
+            }
+            else {
+                return false;
+            }
         }
 };
-
-int main () {
-
-}
