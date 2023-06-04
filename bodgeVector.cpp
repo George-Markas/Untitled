523cf9fc8966b74069cbdc6c377b51bebe685704
George-Markas <georgemarkanton@proton.me>
2023-06-04 22:24:55 +0300

    slightly optimized

--- bodgeVector.cpp
+++ bodgeVector.cpp
@@ -7,44 +7,58 @@ template <typename T> class MyVector {
         unsigned filledSlots;
 
     public:
-        explicit MyVector(unsigned int size) {
+         explicit MyVector(unsigned int size) {
+            if(size != 0) {
+                vectorPtr = new T(size);
+            }
             this->size = size;
-            filledSlots = -1;
-            vectorPtr = new T(size);
+            filledSlots = 0;
         }
 
         void back_push(T toBeInserted) {
-            filledSlots++;
-
-            // If the vector's full, backup contents and remake it with a size increase of 1, transfer contents back.
-            if(filledSlots >= size) {
-                T *bufferPtr = new T(size);
+             // If the vector's full, backup contents and remake it with a size increase of 1, transfer contents back.
+            if(filledSlots == size) {
+                T *bufferPtr = new T(size + 1);
                 memcpy(bufferPtr, vectorPtr, filledSlots*sizeof(T));
                 delete [] vectorPtr;
-                vectorPtr = new T(size + 1);
-                memcpy(vectorPtr, bufferPtr, filledSlots*sizeof(T));
-                delete bufferPtr;
+                vectorPtr = bufferPtr;
                 size++;
             }
             vectorPtr[filledSlots] = toBeInserted;
+            filledSlots++;
         }
 
         T operator[](unsigned int pos) {
             return vectorPtr[pos];
         }
 
+        void print() {
+             for (int i = 0; i < size; i++) {
+                 std::cout << vectorPtr[i] << std::endl;
+             }
+             std::cout << std::endl;
+        }
+
 };
 
 // Driver Code
 int main() {
-    MyVector <int> sampleVector (1);
+    MyVector<int> intVector (4);
 
-    // Pushing in an integer demo.
-    sampleVector.back_push(1337);
+    // Pushing in integers demo.
+    intVector.back_push(1);
+    intVector.back_push(2);
+    intVector.back_push(3);
+    intVector.back_push(4);
 
-    // Dynamic reallocation demo.
-    sampleVector.back_push(256);
+    // Exceeding size at this point, dynamic increase.
+    intVector.back_push(5);
+    intVector.print();
 
-    // Overloaded operator demo
-    std::cout << sampleVector[1];
+    MyVector <bool> boolVector (2);
+    boolVector.back_push(true);
+    boolVector.back_push(true);
+    boolVector.back_push(false);
+
+    boolVector.print();
 }
